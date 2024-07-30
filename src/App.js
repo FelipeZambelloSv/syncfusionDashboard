9508c282901ccab6749f67e79adc8a3af6bfa96d
FelipeZambelloSv <rm99843@fiap.com.br>
2024-07-30 10:16:38 -0300

    feat: complete setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import './App.css'
+
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <h1 className='underline text-3xl'>
+      App
+    </h1>
   )
 }
 
